9657ba280ff4254870fef7c4d2d2e6bef56019f6
juwitt <julia.witton@fu-berlin.de>
2024-02-25 01:06:39 +0100

    rebuilt standardization (by observed maximum instead of theoretical) after reading more papers. adjusted documentation

--- satisficingr/R/meanRootPairs.R
+++ satisficingr/R/meanRootPairs.R
@@ -8,7 +8,13 @@
 #'
 #'
 #' @description
-#' Function calculates the mean of square root differences of all pairs of values in an item package per person (temp_index). Their theoretical range is 0-1, depending on the given data the observed maximum can be lower than 1. For this temp_index, a 0 represents absolute nondiffentiation, meaning that all obsverved values are equal. The temporary indices are standardized then, which means that they are extended to a range of 0-1. For the final_index, 0 means zero nondifferentiation whereas 1 represents the dataset's maximum of nondifferentiation. The procedure is based on the description of Kim et al., 2019, some adjustments were made. The function is applicable to a list of several item packages. The item packs should be named in the following form: scalename.items. The function automatically sets the value of a respondent NA if more than 10% of the items' values are < 0. The proportion is adjustable. The function does not have an explicite rule included for dealing with NAs, yet. I recommend to set NA's < 0. Takes a good while due to included for-loop. Prints durations per item package to the console. For acceleration of the computation, all columns of interest are made numerics before the calculation starts (i.e. unlabelled).
+#' Function calculates the mean of the root of differences of all pairs of values in an item package (rowwise, resulting in `temp_index`). Their theoretical range is 0-1, depending on the given data, the observed maximum can be lower than 1.
+#' For temp_index, a 0 represents absolute nondiffentiation, meaning that all obsverved values are equal.
+#' The temporary indices are standardized in the next step which means that they are extended to a range of 0-1 using the observed maximum of temp_index. For the final_index, 0 means zero nondifferentiation whereas 1 represents the maximum of nondifferentiation. The resulting value is the ratio of the individual variation in comparison to the observed maximum variation.
+#' The procedure is based on the description of Mulligan et al., 2001, described by Kim et al., 2019, Chang & Krosnick, 2009, and others.
+#' One major adjustments was made compared to the formula published by Kim et al. and Chang & Krosnick, 2009: Only one square root is calculated for the sum of all differences between pairs. Otherwise, the temp_indices exceed the substantially interpretable range of 0-1.
+#' The function is applicable to a list of vectors containing item packages. The item packs should be named in the following form: scalename.items. The function automatically sets the temp_index of a respondent NA if more than 10% of the items' values are < 0 (thereby indicating a missing). The proportion is adjustable. The function does not have an explicite rule included for dealing with NAs, yet. I recommend to set NA's < 0.
+#' For acceleration of the computation, all columns of interest are being tranformed to numerics before the calculation starts (i.e. columns are being unlabelled).
 #'
 #'
 #'
@@ -16,7 +22,12 @@
 #' @export
 #'
 #' @author Julia Witton
-#' @references Kim, Y., Dykema, J., Stevenson, J., Black, P., & Moberg, D. P. (2019). Straightlining: Overview of Measurement, Comparison of Indicators, and Effects in Mail–Web Mixed-Mode Surveys. Social Science Computer Review, 37(2), 214–233. https://doi.org/10.1177/0894439317752406
+#' @references
+#' Mulligan, K., Krosnick, J. A., Smith, W., Green, M., & Bizer, G. (2001). Nondifferentiation on Attitude Rating Scales: A Test of Survey Satisflcing Theory Manuscript under review.
+#' Kim, Y., Dykema, J., Stevenson, J., Black, P., & Moberg, D. P. (2019). Straightlining: Overview of Measurement, Comparison of Indicators, and Effects in Mail–Web Mixed-Mode Surveys. Social Science Computer Review, 37(2), 214–233. https://doi.org/10.1177/0894439317752406.
+#' Chang, L., & Krosnick, J. A. (2009). National Surveys Via Rdd Telephone Interviewing Versus the Internet. Public Opinion Quarterly, 73(4), 641–678. https://doi.org/10.1093/poq/nfp075
+#' Fricker, S. (2005). An Experimental Comparison of Web and Telephone Surveys. Public Opinion Quarterly, 69(3), 370–392. https://doi.org/10.1093/poq/nfi027
+#'
 #'
 #' @examples
 #' require(data.table) #generation of example data
@@ -89,7 +100,7 @@ meanRootPairs <- function(df, items, missings.threshold = 0.10) {
     library(dplyr)
 
 
-    # in case data is labelled, the transormation to numeric saves a lot of time
+    # in case data is labelled, the transformation to numeric saves a lot of time
     vector_list <- items
     all_item_names <- unlist(vector_list)
     df <-
@@ -98,7 +109,9 @@ meanRootPairs <- function(df, items, missings.threshold = 0.10) {
     # get the name of the itempackage: first element of the vector in the list (only strings, no numbers)
     item_name <- gsub("\\d", "", items[[1]][1])
 
+    # introduce and define all names of new variables
     temp_index_name <- paste0("temp_index_", item_name)
+    temp_index_max <- paste0(item_name, "_temp_max")
     final_index_name <- paste0("final_index_", item_name)
 
 
@@ -130,15 +143,26 @@ meanRootPairs <- function(df, items, missings.threshold = 0.10) {
 
           }
 
-        ),
+        )) %>%
 
-        # calculate the final index if there is a temporary index by...
-        !!final_index_name := case_when(is.na(.data[[temp_index_name]]) ~ NA,
+      ungroup() %>%
 
-                                        # standardization using the thereoretical maximum and minimum of temp_index
-                                        TRUE ~ (.data[[temp_index_name]] - 1) / (0 - 1))
+      # Create a new column for the maximum of temp_index_name
+      mutate(!!temp_index_max := max(!!rlang::parse_expr(temp_index_name), na.rm = TRUE)) %>%
 
+      # Go back rowwise
+      rowwise() %>%
+
+      # Calculate the new value for final_index_name
+
+      mutate(
+        # paste the temp_index_name and substract from it the pasted temp_index_max dividing this by
+        !!final_index_name := ((!!rlang::parse_expr(temp_index_name) - !!rlang::parse_expr(temp_index_max))/
+                                 # minus the pasted temp_index_maximum
+                                 (0 - !!rlang::parse_expr(temp_index_max)))
       )
+
+
   }
 
   t1 <- Sys.time()
@@ -160,7 +184,6 @@ meanRootPairs <- function(df, items, missings.threshold = 0.10) {
 
   return(df)
 }
-
 
 
 
